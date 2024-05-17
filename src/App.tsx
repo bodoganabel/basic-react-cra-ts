d2a6f7553f6b2f338cfd3d1cb68c12082c3c3138
bodoganabel <bodoganabel@gmail.com>
2024-05-17 23:54:00 +0200

    Display users

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+interface ICustomer {
+  id: string;
+  name: string;
+  email: string;
+}
+
+// Simulating a fetch function that returns customer data
+const fetchCustomers = ():Promise<ICustomer[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 'asdfmn1', name: 'John Doe', email: 'john.doe@example.com' },
+        { id: '1m1vbfo', name: 'Jane Smith', email: 'jane.smith@example.com' },
+        { id: 'sfx44kr', name: 'Jane Smith3', email: 'jane.smith2@example.com' }
+      ]);
+    }, 1000);
+  });
+};
+
+
 function App() {
+
+  const [customers,setCustomers] = useState<ICustomer[]>([]);
+  
+  useEffect(() => { 
+    const getCustomersAsync = async () => {  
+      const getCustomersResult = await fetchCustomers();
+      setCustomers(getCustomersResult);
+    
+    };
+    getCustomersAsync();
+
+
+   }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {customers.map((customer) => <div key={customer.id}>{customer.email}</div>)}
       </header>
     </div>
   );
 }
 
 export default App;
+
+/* TODO: 
+- useCustomers
+- Customers display component
+- Types into separate file
+- useAsyncEffect/useEffectAsync - cleaner code
+- loading indicator
+*/
